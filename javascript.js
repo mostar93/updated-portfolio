982ea164fd80e7d78999d4f4d52e712ecb0a03aa
mostar <mominahmad93@gmail.com>
2018-08-19 12:30:08 -0500

    added links for linkedin and github

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-$(".btn").on("click", function(){
-  
+// var close = true;
+
+$(".btn").on("click", function(event){
+    // event.preventDefault();
     $("#box1").attr("class", "collapse");
     $("#box2").attr("class", "collapse");
     $("#box3").attr("class", "collapse");
     $("#box4").attr("class", "collapse");
+    // return false;
 })
